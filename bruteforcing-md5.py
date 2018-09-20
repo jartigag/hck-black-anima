c9ae1cabb50cbb3ba5cf161f4e877c58576566f4
jartigag <jartigag@protonmail.com>
2018-09-20 22:09:08 +0200

    bruteforcing-md5.py OK

--- bruteforcing-md5.py
+++ bruteforcing-md5.py
@@ -6,9 +6,6 @@
 #
 # testing: echo -n "flag{xx}" | md5sum | xargs -tn 1 python3 bruteforcing-md5.py
 
-#FIXME: not all combinations are generated
-#("found! the flag is flag{99a}", but flag{aab} isn't found)
-
 __author__ = "@jartigag"
 __version__ = "0.1"
 
@@ -19,38 +16,31 @@ import argparse
 import signal #EXTRA(just better looking)
 import sys #EXTRA(just better looking)
 
+def increment(var):
+	# this script was really helpful: https://gist.github.com/pazdera/1121315
+	if len(var) <= 0: # increment(var[1:]) -> if len(var[1:]) <= 0
+	#                                         (eventually it's -1)
+		var.append(chars[0])
+	else:
+		var[0] = chars[(chars.index(var[0]) + 1) % len(chars)]
+		if chars.index(var[0]) is 0:
+			return list(var[0]) + increment(var[1:])
+	return var
+
 def md5(var):
 	return hashlib.md5(var.encode('utf-8')).hexdigest()
 
-def check(var,hash):
+def check(var,hash,verbose,reallyVerbose):
 	flag = 'flag{{{}}}'.format(''.join(var)) # ''.join(var) to get it as string
-	# print(flag)#DEBUGGING
+	if reallyVerbose:
+		print(flag)
+	elif verbose and ''.join(var)==len(var)*'a': # flag{a},flag{aa},flag{aaa},..
+		print(flag)
 	if md5(flag)==hash:
 		print('found! the flag is',flag)
 		bye(0,0)
 	else:
 		return True
-
-def force(hash):
-	pos=0
-	# short=0.001 #DEBUGGING
-	# long=1 #DEBUGGING
-	# slower=100 #DEBUGGING
-	while check(x,hash):
-		x[pos] = chars[ (chars.index(x[pos])+1) % len(chars) ]
-		if x[pos]=='a': #iterated through all chars in this position
-			if len(x)>1:
-				x[pos+1] = chars[ (chars.index(x[pos+1])+1) % len(chars) ]
-				if x[pos+1]=='a':
-					x.append('a')
-					# sleep(long) #DEBUGGING
-					# short=short*slower #DEBUGGING
-			else:
-				pos+=1
-				x.append('a')
-				pos=0
-		# sleep(short)#DEBUGGING
-	print('finished without success..')
 
 def bye(signal, frame):	#this function is a handler for SIGINT
 	print("\nbye!")
@@ -63,15 +53,27 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description="just brute-forcing md5,\
 	 v%s by @jartigag" % __version__)
 	parser.add_argument('hash')
+	parser.add_argument('-m','--maxChars',type=int,default=7)
+	parser.add_argument('-v','--verbose',action='store_true')
+	parser.add_argument('-V','--reallyVerbose',action='store_true')
 	args = parser.parse_args()
 
-	x = ['a'] # the target string is treated as a list
 	chars = string.ascii_letters+string.digits
-	maxChars = 7
 
-	for c in args.hash:
-		if c not in chars:
-			print(c,'is an invalid character')
-			bye(0,0)
+	# validate user input:
+	try:
+		int(args.hash,16)
+	except:
+		print('please enter a valid hash (32 hex digits)')
+		sys.exit(0)
+	if len(args.hash)!=32:
+		print('please enter a valid hash (32 hex digits)')
+		sys.exit(0)
 
-	force(args.hash)
+	# the real job:
+	x = []
+	while len(x)<=args.maxChars:
+		x = increment(x)
+		check(x,args.hash,args.verbose,args.reallyVerbose)
+	print('tried until {i} chars and finished without success..'\
+		.format(args.maxChars))
